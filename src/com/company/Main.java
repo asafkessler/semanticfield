5fcf0043f070160541fe45113587fa15533be2a9
Asaf Kessler <asafkesslerofficial@gmail.com>
2024-11-07 01:28:05 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    /**
+     * זה רעיון למדבר את העולם האסוציסציות האנושי פסיכולוגי כשדה סנטי.
+     * @param args
+     */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
